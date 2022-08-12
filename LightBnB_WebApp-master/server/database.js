1a7802f1ef90703826ba90a2c476e65a64fa3a03
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-08-11 20:20:11 -0400

    refactored getAllResrvations function in database.js

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -27,7 +27,7 @@ const getUserWithEmail = function(email) {
   return pool.query(sql, value)
     .then((result) => {
       console.log(result.rows);
-      return result.rows;
+      return result.rows[0];
     })
     .catch((err) => {
       console.log(err.message);
@@ -50,7 +50,7 @@ const getUserWithId = function(id) {
   return pool.query(sql, value)
     .then((result) => {
       console.log(result.rows);
-      return result.rows;
+      return result.rows[0];
     })
     .catch((err) => {
       console.log(err.message);
@@ -89,8 +89,26 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const sql = `SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT 10;`;
+  const value = [guest_id];
+
+  return pool.query(sql, value)
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
+  
 exports.getAllReservations = getAllReservations;
 
 /// Properties
